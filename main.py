538281d5db947f89edc2bfc0f541e690d890dbb5
yamakon <askkyodai@gmail.com>
2021-12-06 06:58:25 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+# CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}'
+
+
+db.create_all()
+
+
+# CREATE RECORD
+new_book = Book(id=1, title='Jujutsu-Kaisen', author="Akutami Gege", rating=10.0)
+db.session.add(new_book)
+db.session.commit()
